f2bc27bf00299a0633b94b7586aeea54e356830c
Shirakura-Kazuki <ok210192@oks.iput.ac.jp>
2024-06-09 23:42:29 +0900

    addpage0609v4

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -53,9 +53,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "d732155b35f69048332eaddaa53f8f63
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "259f551b56e5693c3d23c7adca443502",
-"/": "259f551b56e5693c3d23c7adca443502",
-"main.dart.js": "5521c4126db1569e13928f03eed6d4f7",
+"index.html": "2f1027a6db69cdef45923336acfee310",
+"/": "2f1027a6db69cdef45923336acfee310",
+"main.dart.js": "9acd3975061ee0dcf06be94b1ab632e6",
 "manifest.json": "8a313bd18c9c92c9fbef36df02ea80dd",
 "version.json": "782e07cc58cbbb8d46c8432c56c58fed"};
 // The application shell files that are downloaded before a service worker can
